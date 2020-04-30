6b0194c91e14bf3f47e19504350399c98017ea8e
Crowdbotics <team@crowdbotics.com>
2020-04-30 05:31:00 +0000

    Initial commit.

---
+++ web_30_dev_3825/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for web_30_dev_3825 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_30_dev_3825.settings')
+
+application = get_wsgi_application()
