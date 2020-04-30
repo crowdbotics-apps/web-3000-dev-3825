7bc4107a8d0b7446d3f54761ef27ba8c7085fe86
Crowdbotics <team@crowdbotics.com>
2020-04-30 08:22:08 +0000

    Update models

--- home/migrations/0003_auto_20200430_0818.py
+++ home/migrations/0003_auto_20200430_0818.py
@@ -6,18 +6,18 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0002_load_initial_data'),
+        ("home", "0002_load_initial_data"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='customtext',
-            name='ccccx',
+            model_name="customtext",
+            name="ccccx",
             field=models.BigIntegerField(blank=True, null=True),
         ),
         migrations.AddField(
-            model_name='customtext',
-            name='ccvc',
+            model_name="customtext",
+            name="ccvc",
             field=models.PositiveSmallIntegerField(blank=True, null=True),
         ),
     ]
--- home/models.py
+++ home/models.py
@@ -8,8 +8,6 @@ from django.db import models
 
 class CustomText(models.Model):
     title = models.CharField(max_length=150,)
-    ccccx = models.BigIntegerField(null=True, blank=True,)
-    ccvc = models.PositiveSmallIntegerField(null=True, blank=True,)
 
     def __str__(self):
         return self.title
